00cb19f7a82015e52935e7787195b261f02f0ff9
bilalzafarr0001 <bilalzafarr39@gmail.com>
2021-10-15 17:02:59 +0500

    firstcommiting...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'quiz-app';
+  title = 'QUIZ APP';
+
+  quizForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.quizForm = this.fb.group({
+      questions: this.fb.array([]),
+    });
+  }
+  questions(): FormArray {
+    return this.quizForm.get('questions') as FormArray;
+  }
+
+  newQuestion(): FormGroup {
+    return this.fb.group({
+      question: '',
+      options: this.fb.array([]),
+    });
+  }
+
+  addQuestion() {
+    console.log('Adding a Question');
+    this.questions().push(this.newQuestion());
+  }
+
+  removeQuestion(index: number) {
+    this.questions().removeAt(index);
+  }
+
+  questionOptions(index: number): FormArray {
+    return this.questions().at(index).get('options') as FormArray;
+  }
+
+  newOption(): FormGroup {
+    return this.fb.group({
+      option: '',
+    });
+  }
+  addQuestionOption(index: number) {
+    this.questionOptions(index).push(this.newOption());
+  }
+  removeQuestionOption(questionIndex: number, optionIndex: number) {
+    this.questionOptions(questionIndex).removeAt(optionIndex);
+  }
+  onSubmit() {
+    console.log(this.quizForm.value);
+  }
+}
+export class country {
+  id: string;
+  name: string;
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
