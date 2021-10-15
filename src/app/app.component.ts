5983b5cd4b3a4c0b5a17dfca9fd04dc3f9fa817f
bilalzafarr0001 <bilalzafarr39@gmail.com>
2021-10-15 18:46:01 +0500

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,11 +55,3 @@ export class AppComponent {
     console.log(this.quizForm.value);
   }
 }
-export class country {
-  id: string;
-  name: string;
-  constructor(id: string, name: string) {
-    this.id = id;
-    this.name = name;
-  }
-}
